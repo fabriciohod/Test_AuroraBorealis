659a156e6a4419418d84ad9ac315120d15b5eb8e
Fabrício Duarte <fabricioohod@outlook.com>
2023-03-30 12:13:51 -0300

    sistema de colldown

---
+++ U_AuroraBorealis/Assets/Scripts/Timer.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace FabriciohodDev
+{
+    [Serializable]
+    public class Timer
+    {
+        [SerializeField] public float durationInSecs;
+        public UnityEvent<float> OnTimerTick;
+
+        public void StartTimer(Action callBackOnEnd = default)
+        {
+            C_StartTimer(callBackOnEnd, durationInSecs);
+        }
+
+        private async void C_StartTimer(Action callback, float duration)
+        {
+            float passedTime = 0f;
+
+            while (passedTime < duration)
+            {
+                passedTime += Time.deltaTime;
+                OnTimerTick?.Invoke(passedTime / duration);
+
+                await Task.Yield();
+            }
+
+            callback.Invoke();
+        }
+    }
+}
